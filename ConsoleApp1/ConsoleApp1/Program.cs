efc53dad14d22cb0f9e73ca50519f43eadab37c1
Paul Grant <paul.grant@ymail.com>
2018-11-18 22:24:09 +0000

    Update console app to be more user friendly.  Update Readme with instructions.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,18 +7,22 @@ namespace PG.AP.Console
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Please enter your array items separated by commas.");
             var intArray = new List<int>();
-            foreach(string arg in args)
+            string foo = System.Console.ReadLine();
+            string[] tokens = foo.Split(",");
+            foreach (string token in tokens)
             {
                 int intValue = 0;
-                if(int.TryParse(arg, out intValue))
+                if(int.TryParse(token, out intValue))
                 {
                     intArray.Add(intValue);
                 }
             }
             var artProg = new PG.AP.Library.ArtProgObject();
             var missingInt = artProg.findTheMissing(intArray.ToArray());
-            var result = missingInt == null ? "No missing item found" : "Missing item = " + missingInt.ToString();
+            var result = missingInt == null ? "No missing item found" : "Missing item = " + string.Join(",", missingInt);
+            System.Console.WriteLine(result);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PG.ArtProg
+
+
+To run the console app - open a console and type the following:
+
+dotnet pg.ap.console.dll
+
+You will then be asked to enter your progressions values, separated by commas.  Hit enter to see the result.
